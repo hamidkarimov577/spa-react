65925fdd7abc90552eb7a323f67dadb7cf7e78df
hamidkarimov577 <hamidkarimov577@gmail.com>
2022-05-19 18:20:38 +0500

    a

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,63 +1,82 @@
-import { useEffect, useState } from "react"
-import {useParams, useHistory} from "react-router-dom"
-import {getMealById} from "../api"
-import Loader from "../components/Loader"
+import { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { getMealById } from "../api";
+import Loader from "../components/Loader";
 
 function Recipe() {
-  const [recipe, setRecipe] = useState([])
-  const [showRecipe, setShowRecipe] = useState(false)
-  const {id} = useParams()
-  const{goBack} = useHistory()
+  const [recipe, setRecipe] = useState([]);
+  const [showRecipe, setShowRecipe] = useState(false);
+  const { id } = useParams();
+  const { goBack } = useHistory();
   const handleRecipeShow = () => {
-    setShowRecipe(!showRecipe)
-  }
+    setShowRecipe(!showRecipe);
+  };
   useEffect(() => {
-    getMealById(id).then(data => setRecipe(data.meals[0]))
-  }, [])
+    getMealById(id).then((data) => setRecipe(data.meals[0]));
+  }, [id]);
   return (
     <>
-      <button className="btn" onClick={goBack}>Go Back</button>
-      {!recipe.idMeal ? <Loader/> : (
+      <button className="btn" onClick={goBack}>
+        Go Back
+      </button>
+      {!recipe.idMeal ? (
+        <Loader />
+      ) : (
         <div className="recipe">
           <img src={recipe.strMealThumb} alt={recipe.strMeal} />
           <h1>{recipe.strMeal}</h1>
-          <h6><b>Category: </b> {recipe.strCategory}</h6>
-          {recipe.strArea  ? <h6><b>Area: </b>{recipe.strArea}</h6>  : null}
+          <h6>
+            <b>Category: </b> {recipe.strCategory}
+          </h6>
+          {recipe.strArea ? (
+            <h6>
+              <b>Area: </b>
+              {recipe.strArea}
+            </h6>
+          ) : null}
           <p>{recipe.strInstructions}</p>
-          <button onClick={handleRecipeShow} className="btn">Show Recipe</button>
+          <button onClick={handleRecipeShow} className="btn">
+            Show Recipe
+          </button>
           {showRecipe ? (
             <table className="table">
-            <thead>
-              <tr>
-                <th>Ingredient</th>
-                <th>Measure</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.keys(recipe).map(key => {
-                if (key.includes('Ingredient') && recipe[key]) {
-                  return (
-                    <tr>
-                      <td>{recipe[key]}</td>
-                      <td>{recipe[`strMeasure${key.slice(13)}`]}</td>
-                    </tr>
-                  )
-                }
-              })}
-            </tbody>
-          </table>
+              <thead>
+                <tr>
+                  <th>Ingredient</th>
+                  <th>Measure</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.keys(recipe).map((key) => {
+                  if (key.includes("Ingredient") && recipe[key]) {
+                    return (
+                      <tr key={key}>
+                        <td>{recipe[key]}</td>
+                        <td>{recipe[`strMeasure${key.slice(13)}`]}</td>
+                      </tr>
+                    );
+                  }
+                })}
+              </tbody>
+            </table>
           ) : null}
-          
+
           {recipe.strYoutube ? (
             <div className="row">
               <h5>Video Recipe</h5>
-              <iframe src={`https://www.youtube.com/embed/${recipe.strYoutube.slice(-11)}`}  allowFullScreen  title={id}/>
+              <iframe
+                src={`https://www.youtube.com/embed/${recipe.strYoutube.slice(
+                  -11
+                )}`}
+                allowFullScreen
+                title={id}
+              />
             </div>
-          ): null}
+          ) : null}
         </div>
       )}
     </>
-  )
+  );
 }
 
 export default Recipe;
